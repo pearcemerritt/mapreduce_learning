f9c78385574c4ff273f452593a07b85d0bb4f98d
Pearce Merritt <pearcemerritt@gmail.com>
2019-07-17 14:40:31 -0700

    initial input

---
+++ add_aliases.bash
@@ -0,0 +1,3 @@
+alias ccc='bin/hadoop com.sun.tools.javac.Main src/AmazeWordCount.java'
+alias hhh='rm -r lotr_output && bin/hadoop jar wc.jar AmazeWordCount lotr_input lotr_output'
+alias jjj='cd src && jar cvf ../wc.jar AmazeWordCount*.class && cd ..'
